c78936fe8e9ab1508118e1ae1eda092f0bd702b9
Martin Bergljung <gravitonian@gmail.com>
2024-11-07 17:58:46 +0100

    Small update to trigger build in Travis, via webhook 4

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-# In-memory storage for events, a little update 3
+# In-memory storage for events, a little update 4
 events = []
 
 # Endpoint to insert a new event
