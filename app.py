8ac35341b43c6291e4ccbf078a5436eec318b2d0
Martin Bergljung <gravitonian@gmail.com>
2024-12-03 14:28:50 +0100

    Added page header + deploy to ECS

--- app.py
+++ app.py
@@ -1,9 +1,29 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, make_response, render_template_string
 
 app = Flask(__name__)
 
-# In-memory storage for events, a little update 7
+# In-memory storage for events
 events = []
+
+# HTML template for displaying events
+html_template = '''
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calendar Events</title>
+</head>
+<body>
+    <h1>Calendar Events</h1>
+    <ul>
+    {% for event in events %}
+        <li>{{ event }}</li>
+    {% endfor %}
+    </ul>
+</body>
+</html>
+'''
 
 # Endpoint to insert a new event
 @app.route('/events', methods=['POST'])
@@ -15,7 +35,7 @@ def add_event():
 # Endpoint to list all events
 @app.route('/events', methods=['GET'])
 def list_events():
-    return jsonify(events), 200
+    return render_template_string(html_template, events=events)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
